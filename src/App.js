74f68dfceedb69c190c32aad29102db095565088
Manish Tuladhar <manish.eclipse@gmail.com>
2020-10-06 14:34:01 +0545

    Event handling and using components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I am a React Developer</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Manish", age: 22 },
+      { name: "Sanjay", age: 29 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Sanjay", age: 29 },
+        { name: "Manish", age: 22 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          Some value
+        </Person>
+      </div>
+    );
+
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi, I'm react developer")
+    // );
+  }
 }
 
 export default App;

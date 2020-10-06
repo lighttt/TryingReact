e55ed74e0dffeda4783860e2228f714b8c9d7e23
Manish Tuladhar <manish.eclipse@gmail.com>
2020-10-06 15:21:43 +0545

    Using react hooks to manage state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const App = (props) => {
+  const [personsState, setPersonState] = useState({
     persons: [
       { name: "Manish", age: 22 },
       { name: "Sanjay", age: 29 },
     ],
-  };
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonState({
       persons: [
         { name: "Sanjay", age: 29 },
         { name: "Manish", age: 22 },
@@ -19,30 +19,28 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I am react app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          Some value
-        </Person>
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hi, I am react app</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        Some value
+      </Person>
+    </div>
+  );
 
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Hi, I'm react developer")
-    // );
-  }
-}
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Hi, I'm react developer")
+  // );
+};
 
 export default App;
